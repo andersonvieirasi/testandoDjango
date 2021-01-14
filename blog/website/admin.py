df63a36e451cb56fbfb40059760cb115409894bf
Anderson <andersonvieira.si@gmail.com>
2021-01-14 08:30:32 -0300

    Atualização 2

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', "created_date",'published_date', "author"]
+    list_display = ['title', "created_date",
+    'published_date', "author", 
+    "categories", "approved"
+    ]
+    search_fields = ['title']
+    ordering = ["title"]
+    
+    def get_queryset(self,request):
+    	return Post.objects.filter(approved=True)
 
 
 
--- blog/website/models.py
+++ blog/website/models.py
@@ -2,13 +2,27 @@ from django.conf import settings
 from django.db import models
 
 
+class Categorias(models.TextChoices):
+    TECH = 'TC', 'Tecnologias'
+    CR = "CR", "Curiosidades"
+    GR = 'GR', "Geral"
+    
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField("Título", max_length=100)
     text = models.TextField("Texto")
+    categories = models.CharField(
+    		"Categoria",
+    		max_length = 2,
+    		choices=Categorias.choices,
+    		default=Categorias.GR,)
+    approved = models.BooleanField("Aprovado", default=True)
+
     created_date = models.DateTimeField("Data da Criação", auto_now_add=True)
     published_date = models.DateTimeField("Data da Publicação", blank=True, 
     	                        null=True, auto_now=True)
 
     def __str__(self):
         return self.title
+
+
